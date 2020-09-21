646d59a7738e6ff3737072f21f2ab186174e279c
hidayatul-rk <71365495+hidayatul-rk@users.noreply.github.com>
2020-09-21 12:08:25 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,12 @@
+var k = "Semangat..."
+var hasil = k.repeat(4)
+	console.log(hasil)
+
+var u = "*"
+	console.log(u.repeat(4))
+	console.log(u.repeat(20))
+	console.log(u.repeat(30))
+	console.log(u.repeat(50))
+	console.log(u.repeat(30))
+	console.log(u.repeat(20))
+	console.log(u.repeat(4))
